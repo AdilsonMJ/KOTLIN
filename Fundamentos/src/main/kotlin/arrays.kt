8bde712ca5be4dbf49be224c8f2dae0ceea96bdb
Adilson Jäger <53977421+AdilsonMJ@users.noreply.github.com>
2021-07-23 18:36:05 +0100

    Initial commit

---
+++ Fundamentos/src/main/kotlin/arrays.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var nomes = arrayOf("Adilson", "Luciani", "Fernanda")
+    println(nomes[1])
+
+    nomes[2] = "Anna"
+    println(nomes[2])
+
+    var numero = intArrayOf(30,40,50,60)
+
+}
